fb399d8d938239d784e6842f192f78b972140db6
Collin Daniel Turner <cdturner@silk2.uvm.edu>
2015-12-08 17:45:03 -0500

    Committing all files from project to GitHub

---
+++ misc/about.php
@@ -0,0 +1,16 @@
+<?php
+//About page of the site. Give info of the people who made it.
+include "top.php";
+?>
+
+<h1>About Us</h1>
+
+<article>
+    <p></p</article>
+
+
+
+<?php
+include "footer.php";
+?>
+
---
+++ misc/index.php
@@ -0,0 +1,13 @@
+<?php
+//##############################################################################
+//
+// main home page for the site 
+// 
+//##############################################################################
+include "top.php";
+// Begin output
+print '<article>';
+print '<p>REALLY ------ this is going to be the very main page.. home page</p>';
+print '</article>';
+include "footer.php";
+?>
